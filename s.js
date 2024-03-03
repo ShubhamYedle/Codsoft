ff1ba1fde5a85a2e5a6d62b313d19d3840df2a03
ShubhamYedle <156688643+ShubhamYedle@users.noreply.github.com>
2024-03-03 09:01:59 +0530

    Delete s.js

--- s.js
+++
@@ -1,9 +0,0 @@
-let string="";
-let buttons=document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    console.log(e.target)
-    string= string+e.target.innerHTML;
-    document.querySelector('input').value=string;
-  })
-})
